7cf13243d3025713fd6f9d13c8d8a80af17f03cc
Matthew Perry <mperry@commerce-architects.com>
2017-07-17 09:27:07 -0700

    sample jQuery

--- public/index.js
+++ public/index.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
  	
  	console.log("client ready");
 
+ 	// GET request the json endpoint and iterate over the 'array'
+ 	$.get("/ca/v1/example/json", function( data ) {
+ 		$(data.array).each(function(number) {
+ 			$(".result").append("<br/>" + number);
+ 		});
+	});
+
 });
